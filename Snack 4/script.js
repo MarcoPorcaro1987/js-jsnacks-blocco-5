81e8e6229ddc900b581647ba2418008c88800d77
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-08 12:13:16 +0100

    fix

--- Snack 4/script.js
+++ Snack 4/script.js
@@ -5,17 +5,13 @@ var numeri = [1,2,3];
 
 // function merge
 function merge(arr1, arr2){
-    var mergedArray = [];
-    while ( mergedArray.length < (arr1.length + arr2.length)){
-        for ( var i = 0; i < arr1.length; i++) {
-            for ( var k = 0; k < arr2.length; k++) {
-                if ( !mergedArray.includes(arr2[i])){
-                    mergedArray.push(arr2[i]);
-                }
-            }
-            mergedArray.push(arr1[i]);
-        }
+    let mergedArray = [];
+    for(let i = 0; i < arr1.length; i++){
+        mergedArray.push(arr1[i]);
+        mergedArray.push(arr2[i]);
     }
     return mergedArray;
 }
+
+
 console.log(merge(lettere, numeri));
